5df758809efb6a16ff64c421867e655e3ba38bef
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-09-10 11:42:45 +0600

    card data given

--- app/components/(site)/home/trainer.js
+++ app/components/(site)/home/trainer.js
@@ -5,10 +5,16 @@ const Trainer = () => {
     return (
         <div className="container">
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
-           <div className="w-full h-[468px] relative custom-shape">
+            <div className="rounded-b-[4px] border border-[#D9D9D9] w-full">
+           <div className="w-full h-[468px]  custom-shape">
             <Image src="/images/home/team.png" alt="team" width={424} height={464} className="h-full w-full object-fill" />
            </div>
-            </div>
+           <div className="ps-[50px]">
+           <h5 className="text-[20px] font-bold mt-[32.28px] font-[Montserrat] line-height-[24.38px]">Alan Bagham</h5>
+           <p className="mt-[24px] text-textcolor2 line-height-[19.5px] font-normal pb-[71px]">Body Building Coach</p>
+           </div>
+           </div>
+           </div>
         </div>
     );
 };
