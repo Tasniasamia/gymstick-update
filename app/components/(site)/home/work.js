800c1413857e541f88b780ae5d7b81aeba0c08b6
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-09-14 01:32:41 +0600

    tomorrow will try

--- app/components/(site)/home/work.js
+++ app/components/(site)/home/work.js
@@ -5,8 +5,20 @@ import { FiChevronsRight } from "react-icons/fi";
 
 const Work = () => {
   return (
-    <div className="container bg-black grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-y-[24px]">
-      <div className="flex w-full">
+    <div className="bg-black py-[150px]">
+    <div className="container  grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-y-[24px]">
+      <div className="flex w-full ">
+        <div className="card-cover">
+        <div className="w-full   bg-[#151515] ">
+         <Image src='/service.png' alt="service" width={100} height={100} className=" h-[60px] w-[60px] object-fill" />
+         <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">User Research</h4>
+         <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">Gathering the Client's Business and Goals</p>
+        </div>
+        <div className="card"></div>
+        </div>
+        <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]"><FiChevronsRight/></div>
+      </div>
+      <div className="flex w-full ">
         <div className="w-full bg-[#151515] ps-[20px] pb-[44px] pt-[20px]">
          <Image src='/service.png' alt="service" width={100} height={100} className="h-[60px] w-[60px] object-fill" />
          <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">User Research</h4>
@@ -46,14 +58,7 @@ const Work = () => {
         </div>
         <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]"><FiChevronsRight/></div>
       </div>
-      <div className="flex w-full">
-        <div className="w-full bg-[#151515] ps-[20px] pb-[44px] pt-[20px]">
-         <Image src='/service.png' alt="service" width={100} height={100} className="h-[60px] w-[60px] object-fill" />
-         <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">User Research</h4>
-         <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">Gathering the Client's Business and Goals</p>
-        </div>
-        <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]"><FiChevronsRight/></div>
-      </div>
+    </div>
     </div>
   );
 };
