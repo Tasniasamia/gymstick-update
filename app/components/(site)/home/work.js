aea6a85dd11da78dbfd433987620e978c749e81c
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-09-15 10:57:41 +0600

    done

--- app/components/(site)/home/work.js
+++ app/components/(site)/home/work.js
@@ -6,10 +6,10 @@ import { FiChevronsRight } from "react-icons/fi";
 const Work = () => {
   return (
     <div className="bg-black py-[150px]">
-      <div className="container  grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-y-[24px]">
-        <div className="flex w-full ">
-          <div className="relative w-full h-full group">
-            <div className="relative z-20 w-[95%] bg-[#151515] ps-[20px] pb-[44px] pt-[20px]">
+      <div className="container  grid lg:grid-cols-3 grid-cols-1 lg:gap-y-[53px]">
+        <div className="flex lg:flex-row flex-col w-full items-center">
+          <div className="relative w-full flex lg:flex-row flex-col justify-center items-center h-[200px] overflow-hidden rounded-[10px]">
+            <div className="relative h-[197px] z-20 w-[99%]  bg-[#151515] ps-[20px] pb-[44px] pt-[20px] rounded-[10px]">
               <Image
                 src="/service.png"
                 alt="service"
@@ -26,112 +26,142 @@ const Work = () => {
             </div>
 
             {/* Centering the red div both horizontally and vertically */}
-            <div className="z-0 group-hover:-rotate-180 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-600 w-full h-full"></div>
+            <div className="card z-0 h-full absolute top-1/2 left-1/2 transform   lg:w-[110%] w-[102%] -translate-x-1/2 -translate-y-1/2"></div>
           </div>
 
-          <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]">
-            <FiChevronsRight />
+          <div className="w-[53px]  text-white lg:h-[197px] h-[53px] flex justify-center items-center text-[20px]">
+            <FiChevronsRight  className="lg:rotate-0 rotate-[90deg]"/>
           </div>
         </div>
-        <div className="flex w-full ">
-          <div className="w-full bg-[#151515] ps-[20px] pb-[44px] pt-[20px]">
-            <Image
-              src="/service.png"
-              alt="service"
-              width={100}
-              height={100}
-              className="h-[60px] w-[60px] object-fill"
-            />
-            <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
-              User Research
-            </h4>
-            <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
-              Gathering the Client's Business and Goals
-            </p>
+
+        <div className="flex lg:flex-row flex-col w-full items-center">
+          <div className="relative w-full flex justify-center items-center h-[200px] overflow-hidden rounded-[10px]">
+            <div className="relative h-[197px] z-20 w-[99%] bg-[#151515] ps-[20px] pb-[44px] pt-[20px] rounded-[10px]">
+              <Image
+                src="/service.png"
+                alt="service"
+                width={100}
+                height={100}
+                className="h-[60px] w-[60px] object-fill"
+              />
+              <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
+                User Research
+              </h4>
+              <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
+                Gathering the Client's Business and Goals
+              </p>
+            </div>
+
+            {/* Centering the red div both horizontally and vertically */}
+            <div className="card z-0 h-full absolute top-1/2 left-1/2 transform   w-[110%]"></div>
           </div>
-          <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]">
-            <FiChevronsRight />
-          </div>
-        </div>
-        <div className="flex w-full">
-          <div className="w-full bg-[#151515] ps-[20px] pb-[44px] pt-[20px]">
-            <Image
-              src="/service.png"
-              alt="service"
-              width={100}
-              height={100}
-              className="h-[60px] w-[60px] object-fill"
-            />
-            <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
-              User Research
-            </h4>
-            <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
-              Gathering the Client's Business and Goals
-            </p>
-          </div>
-          <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]">
-            <FiChevronsRight />
+
+          <div className="w-[53px] text-white lg:h-[197px] h-[53px] flex justify-center items-center text-[20px]">
+          <FiChevronsRight  className="lg:rotate-0 rotate-[90deg]"/>
           </div>
         </div>
-        <div className="flex w-full">
-          <div className="w-full bg-[#151515] ps-[20px] pb-[44px] pt-[20px]">
-            <Image
-              src="/service.png"
-              alt="service"
-              width={100}
-              height={100}
-              className="h-[60px] w-[60px] object-fill"
-            />
-            <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
-              User Research
-            </h4>
-            <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
-              Gathering the Client's Business and Goals
-            </p>
+        <div className="flex lg:flex-row flex-col w-full items-center">
+          <div className="relative w-full flex justify-center items-center h-[200px] overflow-hidden rounded-[10px]">
+            <div className="relative h-[197px] z-20 w-[99%] bg-[#151515] ps-[20px] pb-[44px] pt-[20px] rounded-[10px]">
+              <Image
+                src="/service.png"
+                alt="service"
+                width={100}
+                height={100}
+                className="h-[60px] w-[60px] object-fill"
+              />
+              <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
+                User Research
+              </h4>
+              <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
+                Gathering the Client's Business and Goals
+              </p>
+            </div>
+
+            {/* Centering the red div both horizontally and vertically */}
+            <div className="card z-0 h-full absolute top-1/2 left-1/2 transform   w-[110%]"></div>
           </div>
-          <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]">
-            <FiChevronsRight />
+       
+        
+        </div>
+        <div className="flex lg:flex-row flex-col w-full items-center">
+        <div className="w-[53px] text-white lg:h-[197px] h-[53px] flex justify-center items-center text-[20px]">
+        <FiChevronsRight  className="lg:rotate-0 rotate-[90deg]"/>
+          </div>
+          <div className="relative w-full flex justify-center items-center h-[200px] overflow-hidden rounded-[10px]">
+            <div className="relative h-[197px] z-20 w-[99%] bg-[#151515] ps-[20px] pb-[44px] pt-[20px] rounded-[10px]">
+              <Image
+                src="/service.png"
+                alt="service"
+                width={100}
+                height={100}
+                className="h-[60px] w-[60px] object-fill"
+              />
+              <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
+                User Research
+              </h4>
+              <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
+                Gathering the Client's Business and Goals
+              </p>
+            </div>
+
+            {/* Centering the red div both horizontally and vertically */}
+            <div className="card z-0 h-full absolute top-1/2 left-1/2 transform   w-[120%]"></div>
+          </div>
+
+          <div className="w-[53px] text-white lg:h-[197px] h-[53px]  flex justify-center items-center text-[20px]">
+          <FiChevronsRight  className="lg:rotate-0 rotate-[90deg]"/>
           </div>
         </div>
-        <div className="flex w-full">
-          <div className="w-full bg-[#151515] ps-[20px] pb-[44px] pt-[20px]">
-            <Image
-              src="/service.png"
-              alt="service"
-              width={100}
-              height={100}
-              className="h-[60px] w-[60px] object-fill"
-            />
-            <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
-              User Research
-            </h4>
-            <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
-              Gathering the Client's Business and Goals
-            </p>
+        <div className="flex lg:flex-row flex-col w-full items-center">
+          <div className="relative w-full flex justify-center items-center h-[200px] overflow-hidden rounded-[10px]">
+            <div className="relative h-[197px] z-20 w-[99%] bg-[#151515] ps-[20px] pb-[44px] pt-[20px] rounded-[10px]">
+              <Image
+                src="/service.png"
+                alt="service"
+                width={100}
+                height={100}
+                className="h-[60px] w-[60px] object-fill"
+              />
+              <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
+                User Research
+              </h4>
+              <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
+                Gathering the Client's Business and Goals
+              </p>
+            </div>
+
+            {/* Centering the red div both horizontally and vertically */}
+            <div className="card z-0 h-full absolute top-1/2 left-1/2 transform   w-[110%]"></div>
           </div>
-          <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]">
-            <FiChevronsRight />
+
+          <div className="w-[53px] text-white lg:h-[197px] h-[53px] flex justify-center items-center text-[20px]">
+          <FiChevronsRight  className="lg:rotate-0 rotate-[90deg]"/>
           </div>
         </div>
-        <div className="flex w-full">
-          <div className="w-full bg-[#151515] ps-[20px] pb-[44px] pt-[20px]">
-            <Image
-              src="/service.png"
-              alt="service"
-              width={100}
-              height={100}
-              className="h-[60px] w-[60px] object-fill"
-            />
-            <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
-              User Research
-            </h4>
-            <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
-              Gathering the Client's Business and Goals
-            </p>
+        <div className="flex lg:flex-row flex-col w-full items-center">
+          <div className="relative w-full flex justify-center items-center h-[200px] overflow-hidden rounded-[10px]">
+            <div className="relative h-[197px] z-20 w-[99%] bg-[#151515] ps-[20px] pb-[44px] pt-[20px] rounded-[10px]">
+              <Image
+                src="/service.png"
+                alt="service"
+                width={100}
+                height={100}
+                className="h-[60px] w-[60px] object-fill"
+              />
+              <h4 className="mt-[16px] text-white text-[24px] font-semibold leading-7 tracking-small">
+                User Research
+              </h4>
+              <p className="text-[16px] text-white mt-[4px] pb-[15px] leading-7 tracking-small">
+                Gathering the Client's Business and Goals
+              </p>
+            </div>
+
+            {/* Centering the red div both horizontally and vertically */}
+            <div className="card z-0 h-full absolute top-1/2 left-1/2 transform   w-[110%]"></div>
           </div>
-          <div className="w-[24px] text-white h-[197px] flex justify-center items-center text-[20px]">
-            <FiChevronsRight />
-          </div>
+       
+         
         </div>
       </div>
     </div>
